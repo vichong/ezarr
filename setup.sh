a495e5ebba5368ba883bb0de47a30153cdc4bdeb
Victor Hong <vichong@gmail.com>
2022-11-16 00:40:32 +1100

    16 nov changes

--- setup.sh
+++ setup.sh
@@ -9,6 +9,9 @@ sudo useradd mylar -u 13005
 sudo useradd prowlarr -u 13006
 sudo useradd qbittorrent -u 13007
 sudo useradd jackett -u 13008
+sudo useradd bazarr -u 13009
+sudo useradd jellyfin -u 13010
+sudo useradd sabnzbd -u 13011
 sudo groupadd mediacenter -g 13000
 sudo usermod -a -G mediacenter sonarr
 sudo usermod -a -G mediacenter radarr
@@ -18,10 +21,13 @@ sudo usermod -a -G mediacenter mylar
 sudo usermod -a -G mediacenter prowlarr
 sudo usermod -a -G mediacenter qbittorrent
 sudo usermod -a -G mediacenter jackett
+sudo usermod -a -G mediacenter bazarr
+sudo usermod -a -G mediacenter jellyfin
+sudo usermod -a -G mediacenter sabnzbd
 
 # Make directories
-sudo mkdir -pv docker/{sonarr,radarr,lidarr,readarr,mylar,prowlarr,qbittorrent,jackett}-config
-sudo mkdir -pv data/{torrents,media}/{tv,movies,music,books,comics}
+sudo mkdir -pv docker/{sonarr,radarr,lidarr,readarr,mylar,prowlarr,qbittorrent,jackett,bazarr,jellyfin,sabnzbd}-config
+# sudo mkdir -pv data/{torrents,media}/{tv,movies,music,books,comics}
 
 # Set permissions
 sudo chmod -R 775 data/
@@ -34,5 +40,8 @@ sudo chown -R mylar:mediacenter docker/mylar-config
 sudo chown -R prowlarr:mediacenter docker/prowlarr-config
 sudo chown -R qbittorrent:mediacenter docker/qbittorrent-config
 sudo chown -R jackett:mediacenter docker/jackett-config
+sudo chown -R bazarr:mediacenter docker/bazarr-config
+sudo chown -R bazarr:mediacenter docker/jellyfin-config
+sudo chown -R bazarr:mediacenter docker/sabnzbd-config
 
 echo "UID=$(id -u)" >> .env
